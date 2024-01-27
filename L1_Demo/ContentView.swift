a2f5ce474ade1ab0d98bc411a83f0c8774d88887
SimarCheemaa <simarc@uci.edu>
2024-01-27 14:25:03 -0800

    Initial Commit

---
+++ L1_Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  L1_Demo
+//
+//  Created by Simar Cheema on 1/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ L1_Demo/L1_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1_DemoApp.swift
+//  L1_Demo
+//
+//  Created by Simar Cheema on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct L1_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
